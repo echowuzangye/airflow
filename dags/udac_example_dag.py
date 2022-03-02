10325e3d26419504fa4a3587168ce9a1bbd199d6
echowuzangye <77416608+echowuzangye@users.noreply.github.com>
2022-03-02 15:08:37 -0500

    Add files via upload

---
+++ dags/udac_example_dag.py
@@ -0,0 +1,138 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+from airflow.operators.postgres_operator import PostgresOperator
+
+
+default_args = {
+    'owner': 'echo',
+    'start_date': datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_retry': False,
+}
+
+dag = DAG('sparkify_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *',
+          catchup=False,
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+# comment after tables are created #
+
+create_tables_task = PostgresOperator(
+    task_id="create_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql='create_tables.sql',
+)
+    
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    provide_context=True,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_events",
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    json="s3://udacity-dend/log_json_path.json",    
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_songs",
+    s3_bucket="udacity-dend",
+    s3_key="song_data",
+    json="auto", 
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songplays",
+    sql_statement = SqlQueries.songplay_table_insert    
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    sql_statement = SqlQueries.user_table_insert,
+    append_data=True
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="songs",
+    sql_statement = SqlQueries.song_table_insert,
+    append_data=True
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="artists",
+    sql_statement = SqlQueries.artist_table_insert,
+    append_data=True
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="time",
+    sql_statement = SqlQueries.time_table_insert,
+    append_data=True
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    redshift_conn_id="redshift",
+    data_check = [
+        {'sql': "SELECT COUNT(*) FROM users WHERE userid IS NULL", 'expected_result': 0},
+        {'sql': "SELECT COUNT(*) FROM artists WHERE artistid IS NULL", 'expected_result': 0},
+        {'sql': "SELECT COUNT(*) FROM songs WHERE songid IS NULL", 'expected_result': 0},
+        {'sql': "SELECT COUNT(*) FROM songplays WHERE playid IS NULL", 'expected_result': 0},
+        {'sql': "SELECT COUNT(*) FROM time WHERE start_time IS NULL", 'expected_result': 0}
+    ]
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> create_tables_task
+create_tables_task >> stage_events_to_redshift
+create_tables_task >> stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+load_songplays_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
